a1dc1ef23d0181f87eae4ac99736a497f39f115d
Sean Kwon <Sean Kwon>
2017-01-04 08:59:04 -0500

    add changes for now

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+/*
+const Section = ({}) => (
+  <div className='paragraph-container'>
+    <p>{this.props.text}</p>
+  </div>
+  );
+  */
 
 class Paragraph extends Component {
   render() {
     return <div className="paragraph-container">
       <p>{this.props.text}</p>
+      {this.children}
     </div>
   }
 }
